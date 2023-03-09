76ea8009aeb5fd577921f96cb198e3c5825ec00f
ChrisYadala <112978751+ChrisYadala@users.noreply.github.com>
2023-03-09 02:28:28 -0500

    added route fetch word local. use using /mongo

---
+++ backend/routes/fetchWordLocal.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+var request = require('request');
+
+
+router.route('/:word').get((req, res) => {
+    var options = {
+        'method': 'GET',
+        'url': 'https://us-east-1.aws.data.mongodb-api.com/app/dictionary-eokle/endpoint/getData?word='+req.params.word,
+        'headers': {
+          'apiKey': ''
+        }
+    };
+    request(options, function (error, response) {
+
+        if (error) throw new Error(error);
+        console.log(response.body)
+        body = JSON.parse(response.body)
+        let htmlstring = "<html> <body> <h1>Word: "+req.params.word+"</h1>"
+
+        for (pos of body.usage){
+            htmlstring = htmlstring.concat('<h3>Part of speech: '+ pos.pos+'</h3>')
+            // console.log(pos.pos)
+            htmlstring = htmlstring.concat('<p>Definitions:'+'</p><ul>')
+
+            for(definition of pos.definitions.slice(0,3)){
+                htmlstring = htmlstring.concat('<li>'+definition.definition.gloss+'</li>')
+                if (definition.examples[0] && definition.examples[0].text){
+                    console.log("exampleusage :"+definition.examples[0].text)
+                    htmlstring = htmlstring.concat('<strong> example usage: </strong>'+definition.examples[0].text)
+                }
+                
+            }
+            htmlstring+='</ul>'
+            
+        }
+
+        res.send(htmlstring+"<body><html>")
+    });
+});
+
+
+module.exports = router;

4b1bd9b854032aac080cc087c6efb81587556023
ChrisYadala <112978751+ChrisYadala@users.noreply.github.com>
2023-03-26 02:48:48 -0400

    Feature standardize backend (#3)
    * standard response from backend
    
    * Integrated api and mongoDB responses. Example sentences added from solr.
    
    ---------
    
    Co-authored-by: Chris Jacinth <ychrisjacinth@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,111 +1,194 @@
-const express = require("express")
-const request = require("request")
+const express = require('express')
 const cors = require('cors');
-const mongoose = require('mongoose');
-
+const axios = require('axios')
 const app = express()
 app.use(cors());
 
-app.get('/:word', (req, response) => {
-    let word = req.params.word
+function handleDictionaryData(word, response, body) {
+    // const word = request.params.word;
+    const responseToReact = { "word": word, "meanings": [] };
 
-    var options = {
-        'method': 'GET',
-        'url': 'https://us-east-1.aws.data.mongodb-api.com/app/dictionary-eokle/endpoint/getData?word=' + req.params.word,
-        'headers': {
+    for (const pos of body.usage) {
+        const meaning = {};
+        // part of speech added to response
+        meaning.pos = pos.pos;
+        meaning.definitions = [];
+
+        // meanings added to response
+        for (const definition of pos.definitions.slice(0, 5)) {
+            const def = {};
+            def.meaning = definition.definition.gloss;
+            def.source = "Wiktionary";
+            if (definition.examples[0] && definition.examples[0].text) {
+                def.usage = definition.examples[0].text;
+            }
+            meaning.definitions.push(def);
+        }
+        
+        // audio data added to response
+        let audio = {};
+        if (pos.audio[0] && pos.audio[0].audioLink) {
+            audio.audioLink = pos.audio[0].audioLink;
+            audio.source = "Wiktionary";
+            meaning.audio = audio;
+        }
+
+        responseToReact.meanings.push(meaning);
+    }
+    response.send(responseToReact);
+}
+
+function handleDictionaryAPI(word, response) {
+    const config = {
+        method: 'get',
+        url: 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
+    };
+
+    axios(config)
+        .then(APIResponse => {
+            if (APIResponse.status != 200 || APIResponse.data.length == 0) {
+                console.log('Erroneous status code' + APIResponse.status)
+                console.log('Entire API response:')
+                console.log(APIResponse)
+                return handleDictionaryError("Word meaning not found", response)
+            }
+
+            // TODO : sort meanings with most number of definitions to determine importance?
+            const responseToReact = { "word": word, "meanings": [] };
+            for (let word of APIResponse.data) {
+                for (let meaning of word.meanings) {
+                    let resMeaning = {}
+                    resMeaning.pos = meaning.partOfSpeech
+                    resMeaning.definitions = []
+
+                    for (let definition of meaning.definitions.slice(0, 5)) {
+                        let resDefinition = {}
+                        resDefinition.meaning = definition.definition
+                        resDefinition.usage = definition.example
+                        resDefinition.source = "apidictionary.dev"
+                        resDefinition.license = word.license
+                        resMeaning.definitions.push(resDefinition)
+                    }
+
+                    if (word.phonetics.length != 0) {
+                        phoneticData = word.phonetics.find(p => p.audio && p.audio.length > 0);
+                        if (phoneticData) {
+                            resMeaning.audio = {}
+                            resMeaning.audio.audioLink = phoneticData.audio
+                            resMeaning.audio.source = phoneticData.sourceUrl || ""
+                        }
+                    }
+
+                    responseToReact.meanings.push(resMeaning)
+                }
+            }
+
+            let config = {
+                method: 'get',
+                url: 'http://localhost:8983/solr/my_core/query?q=text:' + word
+            }
+
+            axios(config)
+                .then(solrResponse => {
+                    if (solrResponse.data.response.numFound > 0) {
+                        // To return first few examples fetched from solr:
+                        // responseToReact.generalExamples = solrResponse.data.response.docs.slice(0, 5).map(({ text }) => text[0])
+
+                        // Find few random docs fetched from solr
+                        const docs = solrResponse.data.response.docs;
+                        const numDocs = docs.length;
+                        const numRandomDocs = Math.min(numDocs, 5);
+                        const randomIndices = new Set();
+
+                        while (randomIndices.size < numRandomDocs) {
+                            randomIndices.add(Math.floor(Math.random() * numDocs));
+                            console.log('in while')
+                        }
+                        const randomDocs = [...randomIndices].map(index => docs[index].text[0]);
+                        responseToReact.generalExamples = randomDocs;
+                    }
+                })
+                .finally(() => {
+                    response.send(responseToReact)
+                })
+        })
+        .catch(error => {
+            handleDictionaryError(error, response);
+        });
+}
+
+function handleDictionaryError(error, response) {
+    console.error(error);
+    response.status(500).send('Word meaning not found');
+}
+
+app.get('/:word', (request, response) => {
+    const word = request.params.word;
+
+    const config = {
+        method: 'get',
+        url: 'https://us-east-1.aws.data.mongodb-api.com/app/dictionary-eokle/endpoint/getData',
+        headers: {
             'apiKey': ''
+        },
+        params: {
+            word: word
         }
     };
 
-    request(options, (error, res) => {
+    axios(config)
+        .then(mongoResponse => {
+            if (mongoResponse.status !== 200 || mongoResponse.data == null || mongoResponse.data == "null") {
+                console.log('not found in database. fallback to API')
+                handleDictionaryAPI(word, response)
+            }
+            else {
+                console.log('found in mongoDB')
+                handleDictionaryData(word, response, mongoResponse.data);
+            }
+        })
+        .catch(error => {
+            handleDictionaryAPI(word, response)
+        });
+});
 
-        if (error) throw new Error(error);
+app.get("/api/:word", (req, response) => {
+    let word = req.params.word
+    let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    let htmlstring = "<html> <body> <h1>Word: " + word + "</h1>"
+    request(url, { json: true }, (err, res, body) => {
+        if (err) return console.log(err)
 
         if (res.statusCode != 200) {
             return response.send("word meaning not found")
         }
 
-        body = JSON.parse(res.body)
-        responseToReact = { "word": word, "meanings": [] }
-
-        for (pos of body.usage) {
-            meaning = {}
-            meaning.pos = pos.pos
-            meaning.definitions = []
-            for (definition of pos.definitions.slice(0, 3)) {
-                def={}
-                def.meaning = definition.definition.gloss
-                if (definition.examples[0] && definition.examples[0].text) {
-                    def.usage = definition.examples[0].text
-                }
-                meaning.definitions.push(def)
-            }
-            audio = {}
-            if(pos.audio[0] && pos.audio[0].audioLink){
-                audio.audioLink = pos.audio[0].audioLink
-                audio.source = "Wiktionary"
-                meaning.audio = audio
-            }
-            
-            responseToReact.meanings.push(meaning)
-        }
-
-        response.send(responseToReact)
-    })
-})
-
-app.get("/api/:word", (req, response)=> {
-    let word = req.params.word
-    let url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word
-    let htmlstring = "<html> <body> <h1>Word: "+word+"</h1>"
-    request(url, {json:true}, (err, res, body)=> {
-        if(err) return console.log(err)
-        console.log("status code is "+res.statusCode)
-        if (res.statusCode!=200){
-            return response.send("word meaning not found")
-        }
-        // response.send(body)
-        // let meanings = body[0].meanings
-        for( let word of body){
-            // console.log(meanings)
-            for( p of word.phonetics){
-                if(p.audio && p.audio!=""){
-                    // if(p.hasOwnProperty('audio') && p.audio!=""){
-                    htmlstring += '<audio controls> <source src="'+p.audio+'" type="audio/mp3"></audio>'
+        for (let word of body) {
+            for (p of word.phonetics) {
+                if (p.audio && p.audio != "") {
+                    htmlstring += '<audio controls> <source src="' + p.audio + '" type="audio/mp3"></audio>'
                     break
                 }
-                // else if(p.text && p.text!=""){
-                //     htmlstring += "<p> IPA: "+p.text+"</p>"
-                // }
             }
 
             for (let meaning of word.meanings) {
-                // console.log(meaning)
-                htmlstring = htmlstring.concat('<h3>Part of speech: '+ meaning.partOfSpeech+'</h3>')
-                // console.log('Word: '+word+'. Part of speech: '+ meaning.partOfSpeech+'\n')
-                
-                htmlstring = htmlstring.concat('<p>Definitions:'+'</p><ul>')
-                // console.log('Definitions:')
-
-                for(let definition of meaning.definitions.slice(0,3)){
-                    htmlstring = htmlstring.concat('<li>'+definition.definition+'</li>')
-                    // console.log(definition.definition+'\n')
+                htmlstring = htmlstring.concat('<h3>Part of speech: ' + meaning.partOfSpeech + '</h3>')
+                htmlstring = htmlstring.concat('<p>Definitions:' + '</p><ul>')
+                for (let definition of meaning.definitions.slice(0, 3)) {
+                    htmlstring = htmlstring.concat('<li>' + definition.definition + '</li>')
                 }
                 htmlstring += "</ul>"
             }
         }
-        response.send(htmlstring+"<body><html>")
-        
+        response.send(htmlstring + "<body><html>")
     })
-    
 })
 
 const fetchWordLocalRouter = require('./routes/fetchWordLocal');
 // const audioRouter = require('./routes/googleAudio');
 
-
 app.use('/mongo', fetchWordLocalRouter);
 // app.use('/audio', audioRouter);
-app.listen(3001, ()=> {
-    console.log("server run OK")
+app.listen(3001, () => {
+    console.log("Node server running on port 3001")
 })

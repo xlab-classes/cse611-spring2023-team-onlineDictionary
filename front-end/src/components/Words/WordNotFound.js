a235e7a84eafbfae242fca785e2b099d0bc4a705
Samar Pratap Singh <samar.singh.saini@gmail.com>
2023-03-28 23:17:04 -0400

    Audio integration and modifications to front end

--- front-end/src/components/Words/WordNotFound.js
+++ front-end/src/components/Words/WordNotFound.js
@@ -1,12 +1,12 @@
 import classes from "./WordNotFound.module.css";
-import Card from '../UI/Card';
-
+import Card from "../UI/Card";
 
 const WordNotFound = (props) => {
   return (
-    <div className={classes.card}>
-     <h1> No results found</h1>
-     
+    <div className={classes.container}>
+      <Card>
+        <h1>No results found</h1>
+      </Card>
     </div>
   );
 };

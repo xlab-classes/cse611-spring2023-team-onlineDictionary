ef4539d17a1f4cf307d13367a9d8fed131ed97d4
Robin Mathew <robin.mathew1908@gmail.com>
2023-03-30 03:26:31 -0400

    TOD api added

--- front-end/src/components/MainInfoCard/MainInfoCard.js
+++ front-end/src/components/MainInfoCard/MainInfoCard.js
@@ -9,7 +9,8 @@ export default class MainInfoCard extends React.Component {
       super(props)
     
       this.state = {
-        data: null
+        WOD: null,
+        TOD: null
       }
     }
 
@@ -21,39 +22,50 @@ export default class MainInfoCard extends React.Component {
             .then((result) => {
                 console.log(result)
                 this.setState({
-                    data: result
+                    WOD: result
                 })
             })
             .catch((error) => console.log(error));
             console.log("completed");
+
+        fetch(`http://localhost:3001/getword/trendingword`)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+                this.setState({
+                    TOD: result
+                })
+            })
+            .catch((error) => console.log(error));
     }
 
     render() {
-        const { data } = this.state
+        const { WOD, TOD } = this.state
         return (
         <>
             <div className={classes.MainCard}>
-                {data && 
+                {WOD && 
                 <div className={classes.MainInfocard}>
                     <InfoCard  
                         title={"Word of the day"} 
-                        word={data.wordoftheDay} 
+                        word={WOD.wordoftheDay} 
                         meaning={"Meaning(v): cause (a person or animal) to go with one by holding them by the hand, a halter, a rope, etc. while moving forward."} 
                         Usage={"USAGE: she emerged leading a bay horse."} 
                         meaning2={"Meaning(n): the initiative in an action."} 
                         Usage2={"USAGE: the US is now taking the environmental lead."}
                         />
                 </div>}
+                {TOD &&
                 <div className={classes.MainInfocard}>
                     <InfoCard  
                         title={"Trending word"} 
-                        word={"Pandemic"} 
+                        word={TOD.trendingWord} 
                         meaning={"Meaning(n): A disease prevalent throughout an entire country, continent, or the whole world"} 
                         Usage={"USAGE: Since the Pandemic started, around 75% of the world's force is now working from home."} 
                         meaning2={"Meaning(adj.): (of a disease) prevalent over a whole country or the world."} 
                         Usage2={"USAGE: pandemic diseases have occurred throughout history."}
                         />
-                </div>
+                </div>}
             </div>
         </>
         )
